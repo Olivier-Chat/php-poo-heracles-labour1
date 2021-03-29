c40fdffdc0e0d986790d808fca10af1d3c934cd5
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-03-29 17:48:10 +0200

    exercice done

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
-
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+require_once 'src/Fighter.php';
+$heracles = new Fighter('🧔 Hercules',20,6);
+$nemeeLion = new Fighter('🦁 Lion de Némée',11,13);
+echo $heracles->getName()." Vie: ".$heracles->getLife().PHP_EOL;
+echo $nemeeLion->getName()." Vie: ".$nemeeLion->getLife().PHP_EOL;
+$count = 0;
+while($nemeeLion->getLife()>0 && $heracles->getLife() >0){
+    $count++;
+    echo 'Round '.$count.": ".PHP_EOL;
+    $heracles->fight($nemeeLion);
+    echo $heracles->getName().' attaque '.$nemeeLion->getName().' qui lui reste plus que '.$nemeeLion->getLife()." pts de vie".PHP_EOL;
+    $nemeeLion->fight($heracles);
+    echo $nemeeLion->getName().' attaque '.$heracles->getName().' qui lui reste plus que '.$heracles->getLife()." pts de vie".PHP_EOL;
+}
+$loser = ($heracles->getLife() <=0)? $heracles : $nemeeLion;
+$winner = ($heracles->getLife() <=0)? $nemeeLion : $heracles;
+echo $loser->getName().' is Dead';
+echo $winner->getName().' wins, il lui reste: '.$winner->getLife()." pts de vie".PHP_EOL;
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,6 +1,97 @@
 <?php
 
+
 class Fighter
 {
+    const MAX_LIFE = 100;
+
+    private string $name;
+    private int $strength;
+    private int $dexterity;
+    private int $damage;
+    private int $life = self::MAX_LIFE;
+
+    public function __construct(string $name,int $strength, int $dexterity)
+    {
+        $this->setName($name);
+        $this->setStrength($strength);
+        $this->setDexterity($dexterity);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): Fighter
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+
+    public function getStrength(): int
+    {
+        return $this->strength;
+    }
+
+    public function setStrength(int $strength): Fighter
+    {
+        $this->strength = $strength;
+        return $this;
+    }
+
+    public function getDexterity(): int
+    {
+        return $this->dexterity;
+    }
+
+    public function setDexterity(int $dexterity): Fighter
+    {
+        $this->dexterity = $dexterity;
+        return $this;
+    }
+
+    public function getLife(): int
+    {
+        return $this->life;
+    }
+
+    public function setLife(int $life): Fighter
+    {
+        $this->life = $life;
+        return $this;
+    }
+
+    public function getDamage(): int
+    {
+        return $this->damage;
+    }
+
+
+    public function setDamage(int $damage): Fighter
+    {
+        $this->damage = $damage;
+        return $this;
+    }
+
+    public function fight(Fighter $fighter):void
+    {
+        $this->setDamage(rand(1,$this->strength));
+        $fighter-> defend($this->damage);
+    }
+    public function defend(int $damage){
+        if ($damage > $this->dexterity){
+            $damage -= $this->dexterity;
+        } else{
+            $damage = 0;
+        }
+        $this->life -= $damage;
+        if ($this->life <0) $this->life = 0;
+
+    }
+
+
+
 
 }
